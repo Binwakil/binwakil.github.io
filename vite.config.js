f6338f7453283813da652abb239b4cfc4e8f5bd8
binwakili <almustaphawakili@gmail.com>
2025-09-28 12:15:20 -0400

    deployed to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // For a user or organization site (like username.github.io), the base path is root.
-  base: '/', 
+  // By removing the 'base' property, Vite will use relative paths for assets.
+  // This is a more robust solution for GitHub Pages.
   plugins: [react()],
 })
-
