d7da3e0e668bcd76508fd016aca6aff6f1d8edd9
binwakili <almustaphawakili@gmail.com>
2025-09-28 12:00:47 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // IMPORTANT: Replace 'your-repo-name' with the actual name of your GitHub repository
-  base: '/Binwakil.github.io/', 
+  // For a user or organization site (like username.github.io), the base path is root.
+  base: '/', 
   plugins: [react()],
 })
 
